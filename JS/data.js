e0edddc8081b2ecc7ac7ee682f2e140b399d3634
Amjad <amjad@gmail.com>
2025-12-19 10:00:14 +0500

    first commit

---
+++ JS/data.js
@@ -0,0 +1,28 @@
+var wc = [
+    {"ID":1,"Year":1999,"Winner":"Australia","POM":"Shane Warne","Image":"Images/1.jfif","File":"JS/dummy.docx","Description":"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vel similique veniam aspernatur iure, temporibus nobis expedita perferendis? Rerum in unde quod voluptates ab sint reprehenderit magni! Quas quis nulla officiis?"} ,
+     {"ID":2,"Year":1992,"Winner":"Pakistan","POM":"Imran Khan","Image":"Images/2.jfif","File":"JS/dummy.docx"} ,
+     {"ID":3,"Year":2023,"Winner":"Australia","POM":"Travis Head","Image":"Images/3.jpg","File":"JS/dummy.docx"} ,
+     {"ID":4,"Year":2019,"Winner":"England","POM":"Ben Stokes","Image":"Images/4.jfif","File":"JS/dummy.docx"} ,
+    {"ID":5,"Year":2024,"Winner":"India","POM":"Bumrah","Image":"Images/5.jfif","File":"JS/dummy.docx"} 
+];
+var odi = [
+    {"ID":1,"Year":1999,"Winner":"Australia","POM":"Shane Warne","Image":"Images/1.jfif"} ,
+     {"ID":2,"Year":1992,"Winner":"Pakistan","POM":"Imran Khan","Image":"Images/2.jfif"} ,
+     {"ID":3,"Year":2023,"Winner":"Australia","POM":"Travis Head","Image":"Images/3.jpg"} ,
+     {"ID":4,"Year":2019,"Winner":"England","POM":"Ben Stokes","Image":"Images/4.jfif"} ,
+    {"ID":5,"Year":2024,"Winner":"India","POM":"Bumrah","Image":"Images/5.jfif"} 
+];
+var test = [
+    {"ID":1,"Year":1999,"Winner":"Australia","POM":"Shane Warne","Image":"Images/1.jfif"} ,
+     {"ID":2,"Year":1992,"Winner":"Pakistan","POM":"Imran Khan","Image":"Images/2.jfif"} ,
+     {"ID":3,"Year":2023,"Winner":"Australia","POM":"Travis Head","Image":"Images/3.jpg"} ,
+     {"ID":4,"Year":2019,"Winner":"England","POM":"Ben Stokes","Image":"Images/4.jfif"} ,
+    {"ID":5,"Year":2024,"Winner":"India","POM":"Bumrah","Image":"Images/5.jfif"} 
+];
+var t20 = [
+    {"ID":1,"Year":1999,"Winner":"Australia","POM":"Shane Warne","Image":"Images/1.jfif"} ,
+     {"ID":2,"Year":1992,"Winner":"Pakistan","POM":"Imran Khan","Image":"Images/2.jfif"} ,
+     {"ID":3,"Year":2023,"Winner":"Australia","POM":"Travis Head","Image":"Images/3.jpg"} ,
+     {"ID":4,"Year":2019,"Winner":"England","POM":"Ben Stokes","Image":"Images/4.jfif"} ,
+    {"ID":5,"Year":2024,"Winner":"India","POM":"Bumrah","Image":"Images/5.jfif"} 
+];
